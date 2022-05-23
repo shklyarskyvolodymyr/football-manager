75baed3bd2d56f1654b9ecb0d577bd58b7849c1b
Volodymyr Shklyarsky <volodysh@amdocs.com>
2022-05-23 18:09:26 +0300

    Updated gradle

--- server/src/test/java/com/pet/intellias/football/manager/server/impl/NIOServerImplTest.java
+++ server/src/test/java/com/pet/intellias/football/manager/server/impl/NIOServerImplTest.java
@@ -1,7 +1,24 @@
 package com.pet.intellias.football.manager.server.impl;
 
 
+import org.junit.jupiter.api.Test;
+
 class NIOServerImplTest {
+    @Test
+    void createServer() {
+    }
+
+    @Test
+    void send() {
+    }
+
+    @Test
+    void accept() {
+    }
+
+    @Test
+    void receive() {
+    }
 
 //    todo will complete when NIO client/server be done
 }
